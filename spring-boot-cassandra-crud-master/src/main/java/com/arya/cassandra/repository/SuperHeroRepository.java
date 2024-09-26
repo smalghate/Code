743994bf89bf5633af344bd3fc05dcd299985cdd
smalghate <smalghate@gmail.com>
2024-09-26 10:50:23 +0530

    Delete spring-boot-cassandra-crud-master directory

--- spring-boot-cassandra-crud-master/src/main/java/com/arya/cassandra/repository/SuperHeroRepository.java
+++
@@ -1,9 +0,0 @@
-package com.arya.cassandra.repository;
-
-import com.arya.cassandra.model.SuperHero;
-import org.springframework.data.cassandra.repository.CassandraRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SuperHeroRepository extends CassandraRepository<SuperHero, Long> {
-}
